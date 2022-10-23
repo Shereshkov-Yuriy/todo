b26b54e63659dc873c39801a49340360709fb298
Shereshkov Yuriy <yuriy.shereshkoff@gmail.com>
2022-10-23 17:42:35 +0300

    lesson 1 user model and base api

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
+from rest_framework.routers import DefaultRouter
+
+from authapp.views import CustomUserModelViewSet
+
+router = DefaultRouter()
+router.register("authapp", CustomUserModelViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="api/")),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
