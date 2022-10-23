0cc83f43a5570959cd47e46a5259a8b65113d0bb
Shereshkov Yuriy <yuriy.shereshkoff@gmail.com>
2022-10-23 18:31:02 +0300

    lesson 1 admin

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from authapp import models as authapp_models
+
+
+@admin.register(authapp_models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
