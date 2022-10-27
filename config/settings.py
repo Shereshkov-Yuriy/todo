e1b816054cd2f2815745df0a4f27aab003c667ae
Shereshkov-Yuriy <90192795+Shereshkov-Yuriy@users.noreply.github.com>
2022-10-27 21:56:05 +0300

    Lesson 2 (#2)
    lesson_2 lesson 2 ready

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,13 @@ SECRET_KEY = "django-insecure-wk$%5+7#gpf3tkq(rmp62uq-v*k&#ylph^w0@q!=1nsp9!7!$e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://192.168.1.37:3000",
+]
 
 # Application definition
 
@@ -35,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "authapp",
 ]
@@ -42,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
---
+++ frontend/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+import UserList from "./components/User";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      'users': [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://192.168.1.37:8000/api/authapp/')
+      .then(response => {
+        const users = response.data
+        this.setState(
+          {
+            'users': users
+          }
+        )
+      }).catch(error => console.log(error))
+
+    // const users = [
+    //   {
+    //     'username': 'user010',
+    //     'first_name': 'Egor',
+    //     'last_name': 'Zubov',
+    //     'birthday_year': 'zubov.e@ru.ru',
+    //   },
+    //   {
+    //     'username': 'user011',
+    //     'first_name': 'Aleks',
+    //     'last_name': 'Dedov',
+    //     'birthday_year': 'dedov.a@ru.ru',
+    //   },
+    // ]
+
+    // this.setState(
+    //   {
+    //     'users': users
+    //   }
+    // )
+  }
+
+  render() {
+    return (
+      <div>
+        <Menu />
+        User List
+        <UserList users={this.state.users} />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App;
