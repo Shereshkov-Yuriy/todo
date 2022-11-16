726d25775ffdf7fccde7a8d52786bf46a1c3ff63
Shereshkov Yuriy <yuriy.shereshkoff@gmail.com>
2022-11-16 14:30:34 +0300

    lesson 4 limit pagination

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Project, Todo
 from .serializers import ProjectModelSerializer, TodoModelSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+    
+    
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
+    pagination_class = TodoLimitOffsetPagination
