8bc289255a57127a0664a790d6f909ebd4ba5f5c
Shereshkov Yuriy <yuriy.shereshkoff@gmail.com>
2022-12-06 16:09:55 +0300

    lesson 10 params and filters

--- config/schema.py
+++ config/schema.py
@@ -30,6 +30,9 @@ class Query(ObjectType):
     all_projects = graphene.List(ProjectType)
     all_todo = graphene.List(TodoType)
     all_users = graphene.List(UserType)
+    todo_by_id = graphene.Field(TodoType, id=graphene.Int(required=True))
+    todo_by_project_id = graphene.List(TodoType, id=graphene.Int(required=False))
+    users_by_project = graphene.List(UserType, title=graphene.String(required=False))
 
     def resolve_all_projects(root, info):
         return Project.objects.all()
@@ -39,6 +42,22 @@ class Query(ObjectType):
 
     def resolve_all_users(root, info):
         return User.objects.all()
+
+    def resolve_todo_by_id(root, info, id):
+        try:
+            return Todo.objects.get(id=id)
+        except Todo.DoesNotExist:
+            return None
+
+    def resolve_todo_by_project_id(root, info, id=None):
+        if id:
+            return Todo.objects.filter(project__id=id)
+        return None
+
+    def resolve_users_by_project(root, info, title=None):
+        if id:
+            return User.objects.filter(project__title=title)
+        return None
 
 
 schema = graphene.Schema(query=Query)
